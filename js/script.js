48f1f23ccb47c916581126409ba08b27bbdda4c1
furvietto <fulvio-94@hotmail.it>
2021-11-19 21:08:15 +0100

    adding bonus

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 // Il programma dovrà chiedere all’utente il numero di chilometri che vuole percorrere e l’età del passeggero.
 let km = parseInt(prompt("kilometri da percorrere"));
-let age = parseInt(prompt("quanti anni hai?"))
+let age = parseInt(prompt("quanti anni hai?"));
 
 console.log("kilometri "+  km);
 console.log("anni "+ age);
@@ -16,12 +16,23 @@ let maggiorenni =((ticket * 40) / 100);
 // va applicato uno sconto del 40% per gli over 65.
 // L’output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo). Questo richiederà un minimo di ricerca.
 
-if (age < 18) {
-    document.getElementById("divo").innerHTML = "il prezzo da pagare è di " + (ticket - minorenni).toFixed(2);
-} else if (age > 65) {
-    document.getElementById("divo").innerHTML = "il prezzo da pagare è di " + (ticket - maggiorenni).toFixed(2);
+if ((isNaN(km)) || (isNaN(age)) || km < 10 || age < 5 ) {
+    alert("attenzione dati sbagliati riprova");
+
 } else {
-    document.getElementById("divo").innerHTML = "il prezzo da pagare è di " + ticket.toFixed(2);
+
+    if (age < 18) {
+        document.getElementById("divo").innerHTML = "il prezzo da pagare è di " + (ticket - minorenni).toFixed(2);
+    } else if (age > 65) {
+        document.getElementById("divo").innerHTML = "il prezzo da pagare è di " + (ticket - maggiorenni).toFixed(2);
+    } else {
+        document.getElementById("divo").innerHTML = "il prezzo da pagare è di " + ticket.toFixed(2);
+    }
 }
+
+console.log(isNaN(km));
+console.log(isNaN(age));
+
+
 
 
